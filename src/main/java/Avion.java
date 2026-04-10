8557b8ed45184acbb138c0cd01b981cd6f209184
Moi_maître_de_l'univers <ewanbkk.charles@gmail.com>
2026-04-10 16:37:57 +0200

    modif

--- src/main/java/Avion.java
+++ src/main/java/Avion.java
@@ -1,5 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Avion {
     private int immatriculation;
     private String modele;
     private int capacite;
+
+    public void affecterVol(int immatriculation, int numeroVol){
+        Vol vol = new Vol();
+        this.immatriculation = immatriculation;
+        vol.numeroVol = numeroVol;
+    }
+
+    public void verifierDisponibilite(int immatriculation, int numeroVol){
+        Vol vol = new Vol();
+        this.immatriculation = immatriculation;
+        vol.numeroVol = numeroVol;
+    }
 }
--- src/main/java/Vol.java
+++ src/main/java/Vol.java
@@ -9,32 +9,22 @@ public class Vol {
     private String dateHeureDepart;
     private String dateHeureArrivee;
     private String Etat;
-    Map<Integer, List<String>> vol = new HashMap<>();
 
     public void planifierVol(int numeroVol, String origine, String destination, String dateHeureDepart, String dateHeureArrivee){
-        List<String> detailsVol = new ArrayList<>();
-        detailsVol.add(origine);
-        detailsVol.add(destination);
-        detailsVol.add(dateHeureDepart);
-        detailsVol.add(dateHeureArrivee);
-        vol.put(numeroVol, detailsVol);
+       this.numeroVol = numeroVol;
+       this.origine = origine;
+       this.destination = destination;
+       this.dateHeureDepart = dateHeureDepart;
+       this.dateHeureArrivee = dateHeureArrivee;
+       this.Etat = "Vol";
 
     }
 
     public void annulerVol(int numeroVol){
-        Map<Integer, List<String>> vol = new HashMap<>();
-        vol.remove(numeroVol);
+        this.numeroVol = numeroVol;
+        this.Etat = "Annulé";
     }
 
-    public void obtenirInfosVol(int numeroVol){
-        Map<Integer, List<String>> vol = new HashMap<>();
-        List<String> detailsVol = vol.get(numeroVol);
-        if(detailsVol != null){
-            System.out.println("Le vol numéro " + numeroVol + " part de " + detailsVol.get(0) + " à destination de " + detailsVol.get(1) + ". Il décolle à " + detailsVol.get(2) + " et atterrit à " + detailsVol.get(3) + ".");
-        } else {
-            System.out.println("Le vol numéro " + numeroVol + " n'existe pas.");
-        }
-    }
 
 
 
